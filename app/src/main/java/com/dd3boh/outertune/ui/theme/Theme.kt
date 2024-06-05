Bage340/OuterTune-VK

commit 2be1fe030ae07480e2d64e8b9ab207dbcc79d5a5

app: Remove update checks

app/src/main/java/com/dd3boh/outertune/ui/theme/Theme.kt
@@ -18,7 +18,7 @@ import androidx.palette.graphics.Palette
 import com.google.material.color.scheme.Scheme
 import com.google.material.color.score.Score
 
-val DefaultThemeColor = Color(0xFFED5564)
+val DefaultThemeColor = Color(0xFF4285F4)
 
 @Composable
 fun OuterTuneTheme(
@@ -33,8 +33,8 @@ fun OuterTuneTheme(
             if (darkTheme) dynamicDarkColorScheme(context).pureBlack(pureBlack)
             else dynamicLightColorScheme(context)
         } else {
-            if (darkTheme) Scheme.dark(themeColor.toArgb()).toColorScheme().pureBlack(pureBlack)
-            else Scheme.light(themeColor.toArgb()).toColorScheme()
+            if (darkTheme) Scheme.dark(DefaultThemeColor.toArgb()).toColorScheme().pureBlack(pureBlack)
+            else Scheme.light(DefaultThemeColor.toArgb()).toColorScheme()
         }
     }
 

app/src/main/java/com/dd3boh/outertune/MainActivity.kt
@@ -130,7 +130,6 @@ class MainActivity : ComponentActivity() {
             playerConnection = null
         }
     }
-    var latestVersion by mutableStateOf(BuildConfig.VERSION_CODE.toLong())
 
     override fun onStart() {
         super.onStart()
@@ -530,7 +529,7 @@ class MainActivity : ComponentActivity() {
                                 YouTubeBrowseScreen(navController, scrollBehavior)
                             }
                             composable("settings") {
-                                SettingsScreen(latestVersion, navController, scrollBehavior)
+                                SettingsScreen(navController, scrollBehavior)
                             }
                             composable("settings/appearance") {
                                 AppearanceSettings(navController, scrollBehavior)
@@ -651,28 +650,15 @@ class MainActivity : ComponentActivity() {
                                             Screens.Playlists.route
                                         )
                                     ) {
-                                        Box(
-                                            contentAlignment = Alignment.Center,
-                                            modifier = Modifier
-                                                .size(48.dp)
-                                                .clip(CircleShape)
-                                                .clickable {
-                                                    navController.navigate("settings")
-                                                }
-                                        ) {
-                                            BadgedBox(
-                                                badge = {
-                                                    if (latestVersion > BuildConfig.VERSION_CODE) {
-                                                        Badge()
-                                                    }
-                                                }
-                                            ) {
-
-                                                Icon(
-                                                    painter = painterResource(R.drawable.settings),
-                                                    contentDescription = null
-                                                )
+                                        IconButton(
+                                            onClick = {
+                                                navController.navigate("settings")
                                             }
+                                        ) {
+                                            Icon(
+                                                painter = painterResource(R.drawable.settings),
+                                                contentDescription = null
+                                            )
                                         }
                                     }
                                 },

app/src/main/java/com/dd3boh/outertune/ui/screens/settings/SettingsScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -17,7 +15,6 @@ import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
-import com.dd3boh.outertune.BuildConfig
 import com.dd3boh.outertune.LocalPlayerAwareWindowInsets
 import com.dd3boh.outertune.R
 import com.dd3boh.outertune.ui.component.IconButton
@@ -27,7 +24,6 @@ import com.dd3boh.outertune.ui.utils.backToMain
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(
-    latestVersion: Long,
     navController: NavController,
     scrollBehavior: TopAppBarScrollBehavior,
 ) {
@@ -73,25 +69,6 @@ fun SettingsScreen(
             icon = { Icon(painterResource(R.drawable.info), null) },
             onClick = { navController.navigate("settings/about") }
         )
-        if (latestVersion > BuildConfig.VERSION_CODE) {
-            PreferenceEntry(
-                title = {
-                    Text(
-                        text = stringResource(R.string.new_version_available),
-                    )
-                },
-                icon = {
-                    BadgedBox(
-                        badge = { Badge() }
-                    ) {
-                        Icon(painterResource(R.drawable.update), null)
-                    }
-                },
-                onClick = {
-                    uriHandler.openUri("https://github.com/DD3Boh/OuterTune/releases/latest")
-                }
-            )
-        }
     }
 
     TopAppBar(

app/src/main/res/drawable/update.xml
@@ -1,9 +1,0 @@
-<vector xmlns:android="http://schemas.android.com/apk/res/android"
-    android:width="24dp"
-    android:height="24dp"
-    android:viewportWidth="960"
-    android:viewportHeight="960">
-    <path
-        android:fillColor="@android:color/white"
-        android:pathData="M480,840Q405,840 339.5,811.5Q274,783 225.5,734.5Q177,686 148.5,620.5Q120,555 120,480Q120,405 148.5,339.5Q177,274 225.5,225.5Q274,177 339.5,148.5Q405,120 480,120Q562,120 635.5,155Q709,190 760,254L760,160L840,160L840,400L600,400L600,320L710,320Q669,264 609,232Q549,200 480,200Q363,200 281.5,281.5Q200,363 200,480Q200,597 281.5,678.5Q363,760 480,760Q585,760 663.5,692Q742,624 756,520L838,520Q823,657 720.5,748.5Q618,840 480,840ZM592,648L440,496L440,280L520,280L520,464L648,592L592,648Z" />
-</vector>

app/src/main/res/values-DE/strings.xml
@@ -264,5 +264,4 @@
     <string name="about">Über</string>
     <string name="app_version">App-Version</string>
 
-    <string name="new_version_available">New version available</string>
 </resources>

app/src/main/res/values-be/strings.xml
@@ -282,5 +282,4 @@
     <string name="about">Аб праграме</string>
     <string name="app_version">Версія праграмы</string>
 
-    <string name="new_version_available">Даступная новая версія</string>
 </resources>

app/src/main/res/values-bn-rIN/strings.xml
@@ -264,5 +264,4 @@
     <string name="about">সম্পর্কিত</string>
     <string name="app_version">অ্যাপ সংস্করণ</string>
 
-    <string name="new_version_available">New version available</string>
 </resources>

app/src/main/res/values-cs/strings.xml
@@ -282,5 +282,4 @@
     <string name="about">O aplikaci</string>
     <string name="app_version">Verze aplikace</string>
 
-    <string name="new_version_available">Je dostupná nová verze</string>
 </resources>

commit 0f886180fa82e46bf79e96ded138c82a912312d7

app: ui: Theme: Fix song based accent colors
 * This got broken after commit 2be1fe030ae07480e2d64e8b9ab207dbcc79d5a5

app/src/main/java/com/dd3boh/outertune/ui/theme/Theme.kt
@@ -18,7 +18,7 @@ import androidx.palette.graphics.Palette
 import com.google.material.color.scheme.Scheme
 import com.google.material.color.score.Score
 
-val DefaultThemeColor = Color(0xFF4285F4)
+val DefaultThemeColor = Color(0xFFED5564)
 
 @Composable
 fun OuterTuneTheme(
@@ -33,8 +33,8 @@ fun OuterTuneTheme(
             if (darkTheme) dynamicDarkColorScheme(context).pureBlack(pureBlack)
             else dynamicLightColorScheme(context)
         } else {
-            if (darkTheme) Scheme.dark(DefaultThemeColor.toArgb()).toColorScheme().pureBlack(pureBlack)
-            else Scheme.light(DefaultThemeColor.toArgb()).toColorScheme()
+            if (darkTheme) Scheme.dark(themeColor.toArgb()).toColorScheme().pureBlack(pureBlack)
+            else Scheme.light(themeColor.toArgb()).toColorScheme()
         }
     }
 